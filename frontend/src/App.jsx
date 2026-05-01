02392e92b2cef37d3c8650beeef0f02a59880c81
dev-787 <notexotic555@gmail.com>
2026-05-01 12:17:09 +0530

    feat(router): add /onboarding route mapped to Onboarding component
    - Import Onboarding from components/Onboarding/Onboarding
    - Register <Route path='/onboarding' element={<Onboarding />} /> in BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './index.scss';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Onboarding from './components/Onboarding/Onboarding';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/onboarding" element={<Onboarding />} />
       </Routes>
     </BrowserRouter>
   );
